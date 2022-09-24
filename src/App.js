50acd9e2b82d07fd6d5d6a2516f52d239680b261
Antonio Cabrera <iamtonycabrera@gmail.com>
2022-09-24 23:09:48 +0200

    Added movies.json to consume its movies array and made the app more dynamic

--- src/App.js
+++ src/App.js
@@ -2,42 +2,29 @@ import logo from './logo.svg';
 import './App.css';
 import Movie from './Movie';
 import PageWrapper from './PageWrapper';
+import moviesJson from './movies.json';
 
 function App() {
 
-  let movies = [
-    {
-      movieImage: "images/uploads/mv1.jpg",
-      movieTitle: "Oblivion - 2012",
-      movieRating: "8.1",
-      movieDuration: "2h21’",
-      movieRelease: "1 May 2015",
-      movieDirector: "Joss Whedon",
-      movieActors: "Robert Downey Jr., Chris Evans, Chris Hemsworth"
-    },
-    {
-      movieImage: "images/uploads/mv2.jpg",
-      movieTitle: "Into the Wild - 2014",
-      movieRating: "7.8",
-      movieDuration: "2h21’",
-      movieRelease: "1 May 2015",
-      movieDirector: "Anthony Russo, Joe Russo",
-      movieActors: "Chris Evans, Samuel L. Jackson, Scarlett Johansson"
-    }
-  ]
+  let movies = moviesJson
 
   return (
     <PageWrapper>
-      <Movie
-        movieImage="images/uploads/mv1.jpg"
-        movieTitle="Oblivion - 2012"
-        movieRating="8.1"
-        movieDuration="2h21’"
-        movieRelease="1 May 2015"
-        movieDirector="Joss Whedon"
-        movieActors="Robert Downey Jr., Chris Evans, Chris Hemsworth">
-        Earth's mightiest heroes must come together and learn to fight as a team if they are to stop the mischievous Loki and his alien army from enslaving humanity...
-      </Movie>
+
+      {movies.map(movie =>
+        <Movie
+          movieImage={movie.movieImage}
+          movieTitle={movie.movieTitle}
+          movieRating={movie.movieRating}
+          movieDuration={movie.movieDuration}
+          movieRelease={movie.movieRelease}
+          movieDirector={movie.movieDirector}
+          movieActors={movie.movieActors}>
+          {movie.movieDescription}
+        </Movie>
+      )}
+
+
     </PageWrapper>
   );
 }
